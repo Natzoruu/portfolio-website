f4f6c2199917dab39c9e7176ea946daf9a915935
Alejandro Sanchez <xxalejozxx@gmail.com>
2025-08-14 13:07:47 -0500

    14/8

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ export const Navbar = () =>{
         <div className="container flex items-center justify-between">
             <a className="text-xl font-bold text-primary flex items-center" href="#hero">
                 <span className="relative z-10">
-                    <span className="text-glow text-foreground">Bachoru</span>Portfolio
+                    <span className="text-glow text-foreground">Alejandro</span>Portfolio
                 </span>
             </a>
             {/* desktop */}
